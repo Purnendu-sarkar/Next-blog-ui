735cce24db94b3e78a216bfcb4be1be73889dbd2
Purnendu Sarkar <dsr102.purnendu@gmail.com>
2025-09-27 17:39:43 +0600

    feat(blogs): add static metadata for AllBlogsPage

--- src/app/(public)/blogs/page.tsx
+++ src/app/(public)/blogs/page.tsx
@@ -1,5 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import BlogCard from "@/components/modules/Blogs/BlogCard";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "All Blogs | Next Blog",
+  description:
+    "Browse all blog posts on web development, Next.js, React, and more. Stay updated with the latest tutorials and articles.",
+};
 
 const AllBlogsPage = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/post`, {
