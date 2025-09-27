d2d992554d5000a219dd9e67121021bc6034bcd9
Purnendu Sarkar <dsr102.purnendu@gmail.com>
2025-09-27 17:37:35 +0600

    feat(blogs): add dynamic metadata generation for blog detail pages

--- src/app/(public)/blogs/[blogId]/page.tsx
+++ src/app/(public)/blogs/[blogId]/page.tsx
@@ -1,6 +1,22 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import BlogDetailsCard from "@/components/modules/Blogs/BlogDetailsCard";
 
+export const generateMetadata = async ({
+  params,
+}: {
+  params: Promise<{ blogId: string }>;
+}) => {
+  const { blogId } = await params;
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/post/${blogId}`);
+  const result = await res.json();
+  const blog = result?.data;
+
+  return {
+    title: blog?.title,
+    description: blog?.content,
+  };
+};
+
 export const generateStaticParams = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/post`);
   const result = await res.json();
