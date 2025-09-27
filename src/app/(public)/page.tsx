3a97da7f95fd30069cb92e83bb95cc5839e07856
Purnendu Sarkar <dsr102.purnendu@gmail.com>
2025-09-27 16:20:11 +0600

    feat(home): add ISR with revalidate and show only 3 featured blogs

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -3,7 +3,11 @@ import BlogCard from "@/components/modules/Blogs/BlogCard";
 import Hero from "@/components/modules/Home/Hero";
 
 export default async function HomePage() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/post`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/post`, {
+    next: {
+      revalidate: 30,
+    },
+  });
   const result = await res.json();
   const blogs = Array.isArray(result.data?.data) ? result.data.data : [];
   console.log(blogs);
@@ -12,8 +16,8 @@ export default async function HomePage() {
     <div>
       <Hero />
       <h2 className="text-center my-5 text-4xl">Featured Posts</h2>
-      <div className="grid grid-cols-3 gap-4">
-        {blogs.map((blog: any) => (
+      <div className="grid grid-cols-3 gap-4 max-w-6xl mx-auto my-5">
+        {blogs.slice(0, 3).map((blog: any) => (
           <BlogCard key={blog?.id} post={blog} />
         ))}
       </div>
